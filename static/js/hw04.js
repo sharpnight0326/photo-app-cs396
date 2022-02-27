33d824ccfc8ff6cad3924844e78cb4c7198e983b
sharpnight0326 <94603508+sharpnight0326@users.noreply.github.com>
2022-02-27 12:52:47 -0600

    Update hw04.js

--- static/js/hw04.js
+++ static/js/hw04.js
@@ -450,7 +450,7 @@ const deleteFollowing = (followingId, elem) =>{
 }
 
 const getUsers = () => {
-    fetch("http://127.0.0.1:5000/api/suggestions")
+    fetch("/api/suggestions")
         .then(response => response.json())
         .then(users => {
             const html = users.map(user2Html).join('\n');
